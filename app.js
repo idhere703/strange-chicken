620332708751a30063c09aa5491235a92577d0d9
Brandon <brandon@gooddonegreat.com>
2016-08-20 20:55:05 -0400

    Abstracted out user.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 let express = require('express');
 let app = express();
-let User = require('./models/User');
-let mongoose = require('mongoose');
+let userUtils = require('./lib/userUtils');
 let bodyParser = require('body-parser');
 let port = process.env.PORT ? process.env.PORT : 3000;
 
@@ -13,27 +12,8 @@ app.get('/isRunning', (req, res) => {
 });
 
 app.post('/addUser', (req, res) => {
-  mongoose.connect('mongodb://localhost/test');
-  let db = mongoose.connection;
-  db.on('error', console.error);
-  db.once('open', () => {
-    let cindy = new User({
-      organization: {
-          name: req.body.orgName || "",
-          orgId: req.body.orgId || 0
-      },
-      fullName: req.body.fullEmpName || "",
-      professionalTitle: req.body.empTitle || "",
-      updated: new Date()
-    });
+  userUtils.addUser(req, res);
 
-    cindy.save((err, thor) => {
-      if (err) return console.error(err);
-      db.close();
-      res.sendStatus(200);
-      console.dir(cindy);
-    });
-  });
 });
 
 
