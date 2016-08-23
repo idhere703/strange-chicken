999c51044ff10da2bdffc2a9a0353c1001d31d2d
Brandon <brandon@gooddonegreat.com>
2016-08-22 22:02:09 -0400

    Added organization props.

--- models/Organization.js
+++ models/Organization.js
@@ -1,4 +1,16 @@
 let mongoose = require('mongoose');
-let organizationSchema = new mongoose.Schema({});
+let organizationSchema = new mongoose.Schema({
+  id: Number,
+  address: {
+    address1: String,
+    address2: String,
+    city: String,
+    state: String,
+    zip: String,
+  },
+  orgType: String,
+  rating: Number,
+  employees: [Number]
+});
 let Organization = new mongoose.model('Organization', organizationSchema);
 module.exports = Organization;
