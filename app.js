f88fff860ddb047eb051b55c06c72d01ca2f7fc4
Brandon <idhere703@gmail.com>
2017-06-05 17:36:38 -0400

    Added passport package and config. Still needs user configuration.

--- app.js
+++ app.js
@@ -1,41 +1,68 @@
 let express = require('express');
+let passport = require('passport');
+let LocalStrategy = require('passport-local').Strategy;
 let app = express();
 let userUtils = require('./lib/userUtils');
 let orgUtils = require('./lib/orgUtils');
 let postUtils = require('./lib/postUtils');
 let bodyParser = require('body-parser');
 let port = process.env.PORT ? process.env.PORT : 8100;
-
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({
+      username: username
+    }, function(err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, {
+          message: 'Incorrect username.'
+        });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, {
+          message: 'Incorrect password.'
+        });
+      }
+      return done(null, user);
+    });
+  }
+));
 
 app.get('/isRunning', (req, res) => {
   res.send('Running');
 });
 
-app.post('/addPost', (req, res) => {
+app.post('/addPost', passport.authenticate('local'), (req, res) => {
   postUtils.addPost(req, res);
 });
 
-app.get('/getPosts', (req, res) => {
+app.get('/getPosts', passport.authenticate('local'), (req, res) => {
   postUtils.getPosts(req, res);
 });
 
-app.get('/getPost/:postId', (req, res) => {
+app.get('/getPost/:postId', passport.authenticate('local'), (req, res) => {
   postUtils.getPosts(req, res);
 });
 
-app.post('/updatePost', (req, res) => {
+app.post('/updatePost', passport.authenticate('local'), (req, res) => {
   postUtils.updatePost(req, res);
 });
 
-app.post('/addUser', (req, res) => {
+app.post('/addUser', passport.authenticate('local'), (req, res) => {
   userUtils.addUser(req, res);
 });
 
-app.post('/addOrg', (req, res) => {
+app.post('/addOrg', passport.authenticate('local'), (req, res) => {
   orgUtils.addOrg(req, res);
 });
 
