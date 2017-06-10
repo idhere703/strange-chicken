6adf7a3d8a414f5dce74e9b2159218d8ee04e575
Brandon <idhere703@gmail.com>
2017-06-10 17:10:18 -0400

    Added comment endpoints and removed passport auth.

--- app.js
+++ app.js
@@ -1,67 +1,33 @@
 let express = require('express');
-let passport = require('passport');
-let LocalStrategy = require('passport-local').Strategy;
 let app = express();
 let userUtils = require('./lib/userUtils');
 let orgUtils = require('./lib/orgUtils');
 let postUtils = require('./lib/postUtils');
+let commentUtils = require('./lib/commentUtils');
 let bodyParser = require('body-parser');
 let port = process.env.PORT ? process.env.PORT : 8100;
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({
-      username: username
-    }, function(err, user) {
-      if (err) {
-        return done(err);
-      }
-      if (!user) {
-        console.log('No user');
-        return done(null, false, {
-          message: 'Incorrect username.'
-        });
-      }
-      if (!user.validPassword(password)) {
-        console.log('Incorrect user password');
-        return done(null, false, {
-          message: 'Incorrect password.'
-        });
-      }
-      return done(null, user);
-    });
-  }
-));
 
 app.get('/isRunning', (req, res) => {
   res.send('Running');
 });
 
-app.post('/login', (req, res) => passport.authenticate('local', {
-  successRedirect: '/isRunning',
-  failureRedirect: '/login',
-})(req, res));
-
-app.post('/addPost', passport.authenticate('local'), (req, res) => {
+app.post('/addPost', (req, res) => {
   postUtils.addPost(req, res);
 });
 
-app.get('/getPosts', passport.authenticate('local'), (req, res) => {
+app.get('/getPosts', (req, res) => {
   postUtils.getPosts(req, res);
 });
 
-app.get('/getPost/:postId', passport.authenticate('local'), (req, res) => {
+app.get('/getPost/:postId', (req, res) => {
   postUtils.getPosts(req, res);
 });
 
-app.post('/updatePost', passport.authenticate('local'), (req, res) => {
+app.post('/updatePost', (req, res) => {
   postUtils.updatePost(req, res);
 });
 
@@ -69,20 +35,32 @@ app.post('/addUser', (req, res) => {
   userUtils.addUser(req, res);
 });
 
-app.post('/removeUser', passport.authenticate('local'), (req, res) => {
+app.post('/removeUser', (req, res) => {
   userUtils.removeUser(req, res);
 });
 
-app.get('/getUser/:userId', passport.authenticate('local'), (req, res) => {
+app.get('/getUser/:userId', (req, res) => {
   userUtils.getUser(req, res);
 });
 
-app.get('/getUsers', passport.authenticate('local'), (req, res) => {
+app.get('/getUsers', (req, res) => {
   userUtils.getUsers(req, res);
 });
 
-app.post('/addOrg', passport.authenticate('local'), (req, res) => {
+app.post('/addOrg', (req, res) => {
   orgUtils.addOrg(req, res);
+});
+
+app.get('/getComment/:commentId', (req, res) => {
+  commentUtils.getComment(req, res);
+});
+
+app.get('/getComments', (req, res) => {
+  commentUtils.getComments(req, res);
+});
+
+app.post('/addComment', (req, res) => {
+  commentUtils.addComment(req, res);
 });
 
 
